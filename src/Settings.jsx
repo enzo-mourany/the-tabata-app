62f3cae6a3b85e2972e6f47f291b4b827838df57
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-06 17:20:36 +0200

    added element settings

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -3,8 +3,21 @@ import React from 'react'
 
 export default function Settings() {
     return (
-        <View>
-            <Text>Settings</Text>
+        <View style={{ justifyContent: 'center', flex: 1, alignItems: 'center' }}>
+            <View style={{ backgroundColor: "red", width: "80%", height: "80%", alignItems: 'center' }}>
+                <View alt="title" style={{ backgroundColor: "grey", width: "80%", alignItems: 'center' }}>
+                    <Text style={{ padding: 20 }}>Settings</Text>
+                </View>
+                <View alt="title" style={{ backgroundColor: "grey", width: "80%", alignItems: 'center', marginTop: 30, borderRadius: 14 }}>
+                    <Text style={{ padding: 20 }}>Mode</Text>
+                </View>
+                <View alt="title" style={{ backgroundColor: "grey", width: "80%", alignItems: 'center', marginTop: 30, borderRadius: 14 }}>
+                    <Text style={{ padding: 20 }}>Sound</Text>
+                </View>
+                <View alt="title" style={{ backgroundColor: "green", width: "80%", alignItems: 'center', marginTop: 30, borderRadius: 14 }}>
+                    <Text style={{ padding: 20 }}>Validate</Text>
+                </View>
+            </View>
         </View>
     )
 }
